93bffb4caa89c7c21ab720882ac3266e03c3406f
Adam Xiao <42500628+adam-xiao@users.noreply.github.com>
2020-09-06 15:54:22 -0400

    learning sequences

---
+++ Sequences/lists_intro.py
@@ -0,0 +1,17 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse_mat"
+                  ]
+
+for part in computer_parts:
+    print(part)
+
+print()
+print(computer_parts[2])
+
+print(computer_parts[0:3])
+print(computer_parts[-1])
+
+# strings are immutabble, lists are not
